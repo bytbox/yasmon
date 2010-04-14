8255ff402b0c3a6674d0f8e37068c50ec93df279
Scott Lawrence <bytbox@gmail.com>
2010-04-14 19:40:00 -0400

    indentation fix in setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from distutils.core import setup
 
 from sysmon import version
 
-setup(	name='modred',
-		version=version,
-		description='yet another system monitor',
-		author='Scott Lawrence',
-		author_email='bytbox@gmail.com',
-		maintainer='Scott Lawrence',
-		maintainer_email='bytbox@gmail.com',
-		url='http://github.com/bytbox/yasm',
-		packages=['sysmon'],
-		scripts=['yasm'],
-		)
+setup(name='modred',
+      version=version,
+      description='yet another system monitor',
+      author='Scott Lawrence',
+      author_email='bytbox@gmail.com',
+      maintainer='Scott Lawrence',
+      maintainer_email='bytbox@gmail.com',
+      url='http://github.com/bytbox/yasm',
+      requires=['PyQt4'],
+      packages=['sysmon'],
+      scripts=['yasm'],
+      )
