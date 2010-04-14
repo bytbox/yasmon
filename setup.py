3799d86bca615a414e083bd18965ae016738e7be
Scott Lawrence <bytbox@gmail.com>
2010-04-14 18:47:19 -0400

    Initial commit.
    * Python package setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+from sysmon import version
+
+setup(	name='modred',
+		version=version,
+		description='yet another system monitor',
+		author='Scott Lawrence',
+		author_email='bytbox@gmail.com',
+		maintainer='Scott Lawrence',
+		maintainer_email='bytbox@gmail.com',
+		url='http://github.com/bytbox/yasm',
+		packages=['sysmon'],
+		scripts=['yasm'],
+		)
