b9ce95e1406fba0d22022547b2c5cda43fd87827
Scott Lawrence <bytbox@gmail.com>
2010-05-22 12:01:41 -0400

    RemoteContact infrastructure

--- sysmon/remote.py
+++ sysmon/remote.py
@@ -20,23 +20,102 @@
 
 """
 
+import thread
+import socket
 from system import *
 
-def get_remote(ip):
+def get_remote(addr):
     """Returns an object representing a remote system.
     """
-    system=RemoteSystem(ip)
+    contact=RemoteContact(addr)
+    system=RemoteSystem(contact)
     return system
 
-def RemoteSystem(System):
+class RemoteContact():
+    """Communicates with a remote machine.
+
+    Objects of this class may be used for communication with the
+    remote machine. The necessary locking is handled automatically, so
+    this class is thread-safe.
+    """
+    def __init__(self,addr,port=61874):
+        """Connects to the remote machine.
+        """
+        #connect
+        sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((addr,port))
+        self._socket=sock
+        self._lock=thread.allocate_lock()
+        self._addr=addr
+        self._port=port
+
+    def query(self,query):
+        """Queries the remote machine.
+
+        This method automatically the necessary locking to be
+        thread-safe. Do NOT acquire this object's lock before calling
+        this method - the method will block and never return.
+        """
+        with self.lock():
+            #get the file
+            f=self.socket().makefile()
+            #send the query
+            f.write("%s\n" % query)
+            #read until final token
+            for line in f:
+                if line=='*DONE':
+                    break
+                print line
+            print "DONE"
+
+    def socket(self):
+        """Returns the backing socket.
+        
+        Because using this socket erases the thread safety, this
+        method should never be used.
+        """
+        return self._socket
+
+    def lock(self):
+        """Returns the backing lock.
+        """
+        return self._lock
+
+    def acquire(self):
+        """Calls acquire on the backing lock.
+        """
+        self._lock.acquire()
+
+    def release(self):
+        """Releases the backing lock.
+        """
+        self._lock.release()
+    
+    def addr(self):
+        """Returns the remote address in use.
+        """
+        return self._addr
+
+    def port(self):
+        """Returns the remote port in use.
+        """
+        return self._port
+
+class RemoteSystem(System):
     """Represents a remote system.
     """
-    def __init__(self,ip="remote"):
+    def __init__(self,contact):
         """Creates an empty remote system.
         
         This constructor does not initialize the remote system - that
         is done with the get_remote method.
         """
-        System.__init__(self,ip)
-        print "test"
+        addr=contact.addr()
+        System.__init__(self,addr)
+        self._contact=contact
+
+    def contact(self):
+        """Returns the backing RemoteContact object.
+        """
+        return self._contact
 
